1bce0d5892f0c9bc63e44641fdfc783cf41be861
sothearo-kay <sothearo.kay@techostartup.center>
2025-11-17 15:38:25 +0700

    feat(dev): add nuxt image and vueuse

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,7 +2,12 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  modules: ["@nuxt/eslint", "@nuxt/fonts"],
+  modules: [
+    "@nuxt/eslint",
+    "@nuxt/fonts",
+    "@nuxt/image",
+    "@vueuse/nuxt",
+  ],
 
   devtools: {
     enabled: true,
