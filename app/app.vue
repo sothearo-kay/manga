3f54b2567c06eec27fdc141ccb31e14a490e18df
sothearo-kay <sothearo.kay@techostartup.center>
2025-11-19 11:47:33 +0700

    feat(seo): add metadata

--- app/app.vue
+++ app/app.vue
@@ -1,3 +1,23 @@
+<script setup lang="ts">
+import { SITE_DESCRIPTION, SITE_NAME } from "~/shared/constants";
+
+const url = useRequestURL();
+
+useSeoMeta({
+  title: SITE_NAME,
+  description: SITE_DESCRIPTION,
+  ogTitle: SITE_NAME,
+  ogDescription: SITE_DESCRIPTION,
+  ogSiteName: SITE_NAME,
+  ogUrl: url.href,
+  twitterCard: "summary_large_image",
+  twitterTitle: SITE_NAME,
+  twitterDescription: SITE_DESCRIPTION,
+  ogImage: `${url.origin}/home-og.jpg`,
+  twitterImage: `${url.origin}/home-og.jpg`,
+});
+</script>
+
 <template>
   <NuxtLoadingIndicator color="red" />
   <NuxtLayout>
---
+++ shared/constants/index.ts
@@ -0,0 +1,2 @@
+export const SITE_NAME = "Mangaread";
+export const SITE_DESCRIPTION = "Search and read your favorite manga online. Discover thousands of manga titles across all genres.";
